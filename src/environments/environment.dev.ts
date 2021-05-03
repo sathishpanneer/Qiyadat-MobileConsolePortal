f2b9ea483815896cb4248a4d123ebf7bec40e8f9
Sathish Kumar P <sathishkumar.panneerselvam@sword-in.com>
2021-05-03 14:23:04 +0530

    new chnages

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     environmentName: 'DEV',
     apiUrl: {
-        systemNotificationApi: 'https://swpc128.gswvss.com:44650/MobileConsoleApi/api/Profile/',
+        systemNotificationApi: 'https://localhost:4490/MobileConsoleApi/api/Profile/',
         appSettingApi : 'https://swpc128.gswvss.com:44650/MobileConsoleApi/api/AppSetting/',
         languageApi : 'https://swpc128.gswvss.com:44650/MobileConsoleApi/api/Language/',
         loginApi: 'https://swpc128.gswvss.com:44650/MobileConsoleApi/api/Account/'
